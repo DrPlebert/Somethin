b12447e747fedaa2dc4b0090def41caea5ce1348
DrPlebert <95437967+DrPlebert@users.noreply.github.com>
2022-05-23 21:07:38 -0600

    Update 9Anime tab title changer.user.js

--- 9Anime tab title changer.user.js
+++ 9Anime tab title changer.user.js
@@ -2,8 +2,8 @@
 // @name         9Anime tab title changer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Taiga stuff
+// @author       Plebert
 // @match        https://9anime.id/watch/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=9anime.me
 // @grant        none
