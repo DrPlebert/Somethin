23777e5a538b937839b70400c2c29358af3bad96
DrPlebert <95437967+DrPlebert@users.noreply.github.com>
2022-05-23 21:06:47 -0600

    Add files via upload

---
+++ 9Anime tab title changer.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         9Anime tab title changer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://9anime.id/watch/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=9anime.me
+// @grant        none
+// ==/UserScript==
+var thejuice
+var regex = /(?:watch\/)(.*)\..+\/(ep-[0-9]+)/ig
+var match
+var juice1
+var juice2
+
+function changetabtitle() {
+    thejuice = window.location.href
+    match = regex.exec(thejuice)
+    juice1 = match[1].replace(/-/g," ")
+    juice2 = match[2].replace("ep-","episode ")
+    document.title = "the juice"
+};
+
+
+
+function changetabtitle1() {
+    thejuice = window.location.href
+    match = regex.exec(thejuice)
+    juice1 = match[1].replace(/-/g," ")
+    juice2 = match[2].replace("ep-","episode ")
+    document.title = juice1+" "+juice2
+};
+
+window.addEventListener("click",changetabtitle());
+
+window.addEventListener('DOMNodeInserted', changetabtitle(), false);
